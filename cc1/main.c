4cb558d365cf0a0e0452dfbec8eb4312c97dd60b
raulbethencourt <raul.bethencourt.pro@gmail.com>
2024-03-20 22:29:52 +0100

    [exos] do first two

--- cc1/main.c
+++ cc1/main.c
@@ -1,4 +1,20 @@
 // Copyright (c) 2023 Raul Behtencourt. All Rights Reserved.
 #include <stdio.h>
 
-int main(void) { printf("Hello world!!"); }
+int main(void) {
+  /* NOTE: K&R Exercise 1-1 */
+  printf("Hello world\n");
+
+  /* NOTE: Exercise RS-1 */
+  printf("Hello world\n");
+  printf("Answer %d\n", 18);
+  printf("Name %s\n", "Sarah");
+
+  /* NOTE: Exercise RS-2 */
+  int usf;
+  printf("Enter US Floor\n");
+  scanf("%d", &usf);
+  printf("EU Floor %d\n", usf - 1);
+  
+  /* 21:13 */
+}
