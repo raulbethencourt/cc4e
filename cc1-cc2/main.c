4f94daf1312ff11d6714a05438a822382f6b611e
raulbethencourt <raul.bethencourt.pro@gmail.com>
2024-03-26 21:58:07 +0100

    [working] make exos until number 7

--- cc1-cc2/main.c
+++ cc1-cc2/main.c
@@ -3,34 +3,55 @@
 
 int main(void) {
     // NOTE: K&R Exercise 1-1
-    printf("Hello world\n");
-    printf("\n");
+    // printf("Hello world\n");
+    // printf("\n");
 
     // NOTE: Exercise RS-1
-    printf("Hello world\n");
-    printf("Answer %d\n", 18);
-    printf("Name %s\n", "Sarah");
-    printf("\n");
+    // printf("Hello world\n");
+    // printf("Answer %d\n", 18);
+    // printf("Name %s\n", "Sarah");
+    // printf("\n");
 
     // NOTE: Exercise RS-2
-    int usf;
-    printf("Enter US Floor\n");
-    scanf("%d", &usf);
-    printf("EU Floor %d\n", usf - 1);
-    printf("\n");
+    // int usf;
+    // printf("Enter US Floor\n");
+    // scanf("%d", &usf);
+    // printf("EU Floor %d\n", usf - 1);
+    // printf("\n");
 
     // NOTE: Exercise RS-3
-    char name[50];
-    printf("Enter name\n");
-    scanf("%50s", name);
-    printf("Hello %s\n", name);
-    printf("\n");
+    // char name[50];
+    // printf("Enter name\n");
+    // scanf("%50s", name);
+    // printf("Hello %s\n", name);
+    // printf("\n");
+    //
+    // // NOTE: Exercise RS-4
+    // char line[300];
+    // printf("Enter line\n");
+    // scanf("%[^\n]300s", line);
+    // printf("Line: %s\n", line);
 
-    // NOTE: Exercise RS-4
-    char line[300];
-    printf("Enter line\n");
-    scanf("%[^\n]300s", line);
-    printf("Line: %s\n", line);
+    // NOTE: Exercise RS-5-1
+    // char line[300];
+    // printf("Enter line\n");
+    // fgets(line, 300, stdin);
+    // printf("Line: %s\n", line);
 
-    /* 41:31 */
+    // NOTE: Exercise RS-5-2
+    // char line[300];
+    // FILE *hand;
+    // hand = fopen("test.txt", "r");
+    // while (fgets(line, 300, hand) != NULL) {
+    //     printf("%s", line);
+    // }
+
+    // NOTE: Exercise RS-6
+    int i;
+    for (i = 0; i < 5; i++) {
+        printf("%d\n", i);
+    }
+    
+    // NOTE: Exercise RS-7
+    // 31:16
 }
