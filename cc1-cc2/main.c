751098d3b71de71ab730d4dd932d91b46ac6adb4
raulbethencourt <raul.bethencourt.pro@gmail.com>
2024-03-30 20:45:16 +0100

    [working] number 7

--- cc1-cc2/main.c
+++ cc1-cc2/main.c
@@ -47,11 +47,49 @@ int main(void) {
     // }
 
     // NOTE: Exercise RS-6
-    int i;
-    for (i = 0; i < 5; i++) {
-        printf("%d\n", i);
-    }
-    
+    // int i;
+    // for (i = 0; i < 5; i++) {
+    //     printf("%d\n", i);
+    // }
+
     // NOTE: Exercise RS-7
-    // 31:16
+    int first = 1;
+    int val, maxval, minval;
+
+    // NOTE: EOF equals to <ctrl-d>
+    while ((val = getchar()) != EOF) {
+        if (first || val > maxval)
+            maxval = val;
+        if (first || val < minval)
+            minval = val;
+        first = 0;
+    }
+
+    printf("Maximum %d\n", maxval);
+    printf("Minimum %d\n", minval);
+
+    // NOTE: Exercise RS-8
+    // int guess;
+    //
+    // while (scanf("%d", &guess) != EOF) {
+    //     if (guess == 42) {
+    //         printf("Nice work!\n");
+    //         break;
+    //     } else if (guess < 42)
+    //         printf("Too low - guess again\n");
+    //     else
+    //         printf("Too high - guess again\n");
+    // }
+
+    // NOTE: Exercise RS-9
+    // int mymult(int a, int b);
+    // int retval;
+    //
+    // retval = mymult(6, 7);
+    // printf("Answer: %d\n", retval);
+}
+
+int mymult(int a, int b) {
+    int c = a * b;
+    return c;
 }
